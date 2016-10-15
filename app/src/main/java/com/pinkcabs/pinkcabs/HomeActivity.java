1fd5e43fd33a1f358f071a34913c3a1e35638afc
Dhruv Rathi <dhruvrathi15@gmail.com>
2016-10-15 14:43:41 +0530

    Home Activity Baby Stage

---
+++ app/src/main/java/com/pinkcabs/pinkcabs/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.pinkcabs.pinkcabs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+
+    }
+}
