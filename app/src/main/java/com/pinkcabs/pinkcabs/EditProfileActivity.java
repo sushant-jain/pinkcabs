80a114dd6353c366ce8e4bb6375c5fca3bddb50e
aanyajindal <aanyajindal05@gmail.com>
2016-10-15 20:38:12 +0530

    Added editPic functionality

--- app/src/main/java/com/pinkcabs/pinkcabs/EditProfileActivity.java
+++ app/src/main/java/com/pinkcabs/pinkcabs/EditProfileActivity.java
@@ -1,12 +1,16 @@
 package com.pinkcabs.pinkcabs;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -16,7 +20,11 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.pinkcabs.pinkcabs.Models.FBUser;
 
+import java.io.IOException;
+
 public class EditProfileActivity extends AppCompatActivity {
+
+    private static final int PICK_IMAGE_REQUEST = 12341;
 
     EditText etName, etContact;
     Button btnEditProfile, btnEditImage;
@@ -57,5 +65,37 @@ public class EditProfileActivity extends AppCompatActivity {
             }
         });
 
+        btnEditImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+
+            }
+        });
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri uri = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                // Log.d(TAG, String.valueOf(bitmap));
+
+                ImageView imageView = (ImageView) findViewById(R.id.iv_image);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
