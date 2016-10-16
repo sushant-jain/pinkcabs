de63019fdeeb61dfd49cedc031dfaed77ab83004
Vishal Wadhwa <vishalwadhwa13@gmail.com>
2016-10-16 05:34:15 +0530

    Added location update service

---
+++ app/src/main/java/com/pinkcabs/pinkcabs/LocationUpdate.java
@@ -0,0 +1,76 @@
+package com.pinkcabs.pinkcabs;
+
+import android.app.Service;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.pinkcabs.pinkcabs.Models.FBUser;
+
+public class LocationUpdate extends Service {
+    private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    private LocationManager manager;
+    private LocationListener listener;
+
+    public LocationUpdate() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        manager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                ServerRequests requests = new ServerRequests();
+
+                requests.updateMyLocation(getApplicationContext(), user.getUid(),
+                        location.getLatitude(), location.getLongitude());
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+        }
+        manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 60 * 1000, 0, listener);
+        return START_STICKY;
+    }
+}
