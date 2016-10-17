2aefb284700e8a14ba50517ee5cec30ab6130ad5
Sushant Jain <sjsushantjain22@gmail.com>
2016-10-17 20:00:31 +0530

    screenshots

--- app/src/main/java/com/pinkcabs/pinkcabs/ServerRequests.java
+++ app/src/main/java/com/pinkcabs/pinkcabs/ServerRequests.java
@@ -25,13 +25,13 @@ import java.util.Map;
 public class ServerRequests {
 
     private RequestCallback callback;
-//    private static ServerRequests requests=null;
-    private String ALL_CABS="http://198.199.120.41/pinkcabs/v1/drivers";
-    private String CABS_WITHIN_6="http://198.199.120.41/pinkcabs/v1/driver/lat=_LAT_/long=_LONG_";
-    private String SELECT_A_DRIVER="http://198.199.120.41/pinkcabs/v1/driver/_DRV_FIREID_/allot";
-    private String RELEASE_A_DRIVER="http://198.199.120.41/pinkcabs/v1/driver/_DRV_FIREID_/release";
-    private String UPDATE_MY_LOCATION="http://198.199.120.41/pinkcabs/v1/user/location/_USER_FIRE_ID_";
-    private String NEW_USER="http://198.199.120.41/pinkcabs/v1/register/user";
+    //    private static ServerRequests requests=null;
+    private String ALL_CABS = "http://198.199.120.41/pinkcabs/v1/drivers";
+    private String CABS_WITHIN_6 = "http://198.199.120.41/pinkcabs/v1/driver/lat=_LAT_/long=_LONG_";
+    private String SELECT_A_DRIVER = "http://198.199.120.41/pinkcabs/v1/driver/_DRV_FIREID_/allot";
+    private String RELEASE_A_DRIVER = "http://198.199.120.41/pinkcabs/v1/driver/_DRV_FIREID_/release";
+    private String UPDATE_MY_LOCATION = "http://198.199.120.41/pinkcabs/v1/user/location/_USER_FIRE_ID_";
+    private String NEW_USER = "http://198.199.120.41/pinkcabs/v1/register/user";
     private static final String TAG = "ServerRequests";
 //
 //    public static ServerRequests getInstance() {
@@ -43,7 +43,7 @@ public class ServerRequests {
         this.callback = callback;
     }
 
-     void getAllCabs(Context ctx) {
+    void getAllCabs(Context ctx) {
         JsonObjectRequest req = new JsonObjectRequest(
                 Request.Method.GET,
                 ALL_CABS,
@@ -53,7 +53,7 @@ public class ServerRequests {
                     public void onResponse(JSONObject response) {
                         try {
                             JSONArray driverList = response.getJSONArray("driver_list");
-                            if (callback!=null) callback.response(driverList);
+                            if (callback != null) callback.response(driverList);
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -62,24 +62,24 @@ public class ServerRequests {
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        if (callback!=null) callback.response(null);
+                        if (callback != null) callback.response(null);
                     }
                 });
         Volley.newRequestQueue(ctx).add(req);
     }
 
-    void getCabsWithin6(Context ctx,double latitude, double longitude) {
+    void getCabsWithin6(Context ctx, double latitude, double longitude) {
         JsonObjectRequest req = new JsonObjectRequest(
                 Request.Method.GET,
-                CABS_WITHIN_6.replace("_LAT_",String.valueOf(latitude))
-                        .replace("_LONG_",String.valueOf(longitude)),
+                CABS_WITHIN_6.replace("_LAT_", String.valueOf(latitude))
+                        .replace("_LONG_", String.valueOf(longitude)),
                 null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
                             JSONArray driverList = response.getJSONArray("driver_list");
-                            if (callback!=null) callback.response(driverList);
+                            if (callback != null) callback.response(driverList);
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -88,23 +88,23 @@ public class ServerRequests {
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        if (callback!=null) callback.response(null);
+                        if (callback != null) callback.response(null);
                     }
                 });
         Volley.newRequestQueue(ctx).add(req);
     }
 
     void selectDriver(Context ctx, String driverFirebaseId, final String userFirebaseId) {
-        StringRequest req=new StringRequest(
+        StringRequest req = new StringRequest(
                 Request.Method.PUT,
                 SELECT_A_DRIVER.replace("_DRV_FIREID_", driverFirebaseId),
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         try {
-                            Log.d(TAG, "onResponse: "+response);
+                            Log.d(TAG, "onResponse: " + response);
                             String res = new JSONObject(response).getString("result");
-                            if (callback!=null) callback.response(res);
+                            if (callback != null) callback.response(res);
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -113,15 +113,15 @@ public class ServerRequests {
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        if (callback!=null) callback.response(null);
-                        Log.d(TAG, "onErrorResponse: "+error.getMessage());
+                        if (callback != null) callback.response(null);
+                        Log.d(TAG, "onErrorResponse: " + error.getMessage());
                     }
                 }
-        ){
+        ) {
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
-                HashMap<String,String> map=new HashMap<>();
-                map.put("user_fireb_id",userFirebaseId);
+                HashMap<String, String> map = new HashMap<>();
+                map.put("user_fireb_id", userFirebaseId);
                 return map;
             }
 
@@ -134,7 +134,7 @@ public class ServerRequests {
     }
 
     void releaseDriver(Context ctx, String driverFirebaseId) {
-        StringRequest req=new StringRequest(
+        StringRequest req = new StringRequest(
                 Request.Method.PUT,
                 RELEASE_A_DRIVER.replace("_DRV_FIREID_", driverFirebaseId),
                 new Response.Listener<String>() {
@@ -142,7 +142,7 @@ public class ServerRequests {
                     public void onResponse(String response) {
                         try {
                             String res = new JSONObject(response).getString("result");
-                            if (callback!=null) callback.response(res);
+                            if (callback != null) callback.response(res);
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -151,7 +151,7 @@ public class ServerRequests {
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        if (callback!=null) callback.response(null);
+                        if (callback != null) callback.response(null);
                     }
                 }
         );
@@ -159,17 +159,17 @@ public class ServerRequests {
     }
 
     void newUser(Context ctx, final String userFirebaseId) {
-        Log.d(TAG, "newUser: "+userFirebaseId);
-        StringRequest req=new StringRequest(
+        Log.d(TAG, "newUser: " + userFirebaseId);
+        StringRequest req = new StringRequest(
                 Request.Method.POST,
                 NEW_USER,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         try {
-                            Log.d(TAG, "onResponse: "+response);
+                            Log.d(TAG, "onResponse: " + response);
                             JSONObject jsonObject = new JSONObject(response);
-                            if (callback!=null) callback.response(jsonObject);
+                            if (callback != null) callback.response(jsonObject);
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -178,15 +178,15 @@ public class ServerRequests {
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Log.d(TAG, "onErrorResponse: "+error.getMessage());
-                        if (callback!=null) callback.response(null);
+                        Log.d(TAG, "onErrorResponse: " + error.getMessage());
+                        if (callback != null) callback.response(null);
                     }
                 }
-        ){
+        ) {
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String,String> map=new HashMap<>();
-                map.put("user_fireb_id",userFirebaseId);
+                Map<String, String> map = new HashMap<>();
+                map.put("user_fireb_id", userFirebaseId);
                 return map;
             }
 
@@ -199,7 +199,7 @@ public class ServerRequests {
     }
 
     void updateMyLocation(Context ctx, String userFirebaseId, final double latitude, final double longitude) {
-        StringRequest req=new StringRequest(
+        StringRequest req = new StringRequest(
                 Request.Method.PUT,
                 UPDATE_MY_LOCATION.replace("_USER_FIRE_ID_", userFirebaseId),
                 new Response.Listener<String>() {
@@ -207,7 +207,7 @@ public class ServerRequests {
                     public void onResponse(String response) {
                         try {
                             String res = new JSONObject(response).getString("result");
-                            if (callback!=null) callback.response(res);
+                            if (callback != null) callback.response(res);
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -216,13 +216,13 @@ public class ServerRequests {
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        if (callback!=null) callback.response(null);
+                        if (callback != null) callback.response(null);
                     }
                 }
-        ){
+        ) {
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String,String> map=new HashMap<>();
+                Map<String, String> map = new HashMap<>();
                 map.put("latitude", String.valueOf(latitude));
                 map.put("longitude", String.valueOf(longitude));
                 return map;
@@ -231,7 +231,7 @@ public class ServerRequests {
         Volley.newRequestQueue(ctx).add(req);
     }
 
-    interface RequestCallback{
+    interface RequestCallback {
         void response(Object data);
     }
 }
